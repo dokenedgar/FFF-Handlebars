a57273c15fd6ba54d0944f5063277f9c2da90739
Doken Edgar <dokenedgarjr@gmail.com>
2018-09-22 03:19:32 +0100

    added eslint and Airbnb styleguide, qpplied it to index.js

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 const express = require('express');
 const app = express();
-const jwt = require('jsonwebtoken')
-const handlebars = require('express-handlebars').create({ defaultLayout:'main' });
+const path = require('path');
+const jwt = require('jsonwebtoken');
+const handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.use(express.json());
 
-let orders = [];
+const orders = [];
 
-let users = [
-  { fname: 'Yakubu', sname: 'Frank', phone: '12345678900', username: 'Franky', pword: 'superfrank' },
-  { fname: 'David', sname: 'McKenxie', phone: '09876543211', username: 'McDave', pword: 'pword' }
+const users = [
+  {
+    fname: 'Yakubu', sname: 'Frank', phone: '12345678900', username: 'Franky', pword: 'superfrank'
+  },
+  {
+    fname: 'David', sname: 'McKenxie', phone: '09876543211', username: 'McDave', pword: 'pword' 
+  }
 ];
-let adminUsers = [
+const adminUsers = [
   { username: 'lionel', password: 'messi' },
   { username: 'sergio', password: 'ramos' }
 ];
-//const initData = require('./initialdata.js');
-let foodList = [
+
+const foodList = [
   { foodName: 'Spaghetti', foodPrice: '350', foodDesc: 'Nicely cooked nigerian-styled spaghetti' },
   { foodName: 'Plantain', foodPrice: '100', foodDesc: 'Fresh plantain fried to the perfect degree' },
   { foodName: 'Pizza', foodPrice: '1000', foodDesc: 'Freshly baked Pizza, with the finest of ingredients' },
@@ -30,8 +35,8 @@ let foodList = [
   { foodName: 'Fried Eggs', foodPrice: '150', foodDesc: 'Quality fried eggs, from healthy chickens with amazing spices' },
   { foodName: 'Bacon', foodPrice: '650', foodDesc: 'Fine bacon from the healthiest of animals' }
 ];
-let messagesToAdmin = [];
-let messagesFromAdmin = [
+const messagesToAdmin = [];
+const messagesFromAdmin = [
   { receiver: 'McDave', message: 'Order received' },
   { receiver: 'Franky', message: 'Your order of Rice and burger of Monday 4th April has been completed. Thank you for using our service' },
   { receiver: 'Franky', message: 'Order accepted' },
@@ -39,65 +44,64 @@ let messagesFromAdmin = [
 ];
 
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, '/public')));
 app.set('port', process.env.PORT || 3000);
 
-//Middleware gets called for all routes starting with /api
-//Inorder to check for the presence of a token
+// Middleware gets called for all routes starting with /api
+// Inorder to check for the presence of a token
 app.use('/api', verifyToken);
 
-app.get('/', function (req, res) {
-	res.render('index', { layout: 'guest' , user: {guest:true}, menu: JSON.stringify(foodList) } );
+app.get('/', (req, res) => {
+  res.render('index', { layout: 'guest', user: { guest: true }, menu: JSON.stringify(foodList) });
 });
 
 
-app.get('/signin', function (req, res) {
-	res.render('signin', { layout: 'guest' });
+app.get('/signin',  (req, res) => {
+  res.render('signin', { layout: 'guest' });
 });
 
 app.get('/signin/:uname/:pword', (req, res) => {
-  let result = { userFound: false };
+  const result = { userFound: false };
   const signInUser = {
     username: req.params.uname, password: req.params.pword
   };
 
-  users.forEach(function (element) {
+  users.forEach((element) => {
     if ((element.username === signInUser.username) && (element.pword === signInUser.password)) {
       result.userFound = true;
-      //Create jwt token for the user
-      jwt.sign( { signInUser }, 'tre-lala', (err, token) => {
-        //result.token = token;
+      // Create jwt token for the user
+      jwt.sign({ signInUser }, 'tre-lala', (err, token) => {
+        // result.token = token;
         result.token = token;
         res.send(result);
-      })
+      });
     }
   });
 
   if (!result.userFound) {
     res.send(result);
   }
-
 });
 
-//===================VERIFY TOKEN========================>
-function verifyToken (req, res, next) {
+// ===================VERIFY TOKEN========================>
+function verifyToken(req, res, next) {
   const bearerHeader = req.headers['authorization'];
   if (typeof bearerHeader !== 'undefined') {
     const bearer = bearerHeader.split(' ');
     const bearerToken = bearer[1];
     req.token = bearerToken;
-    //Add verification of the token here as well.
-    //Such that when the route is finally reached, just send back the data
+    // Add verification of the token here as well.
+    // Such that when the route is finally reached, just send back the data
     next();
   }
   else {
     res.sendStatus(403);
   }
 }
-//=======================================================>
+// =======================================================>
 
-app.get('/signup', function (req, res) {
-	res.render('signup', { layout: 'guest' });
+app.get('/signup', (req, res) => {
+  res.render('signup', { layout: 'guest' });
 });
 
 app.post('/signup', (req, res) => {
@@ -108,174 +112,164 @@ app.post('/signup', (req, res) => {
   res.send(newUser);
 });
 
-app.get('/logout', function (req, res) {
+app.get('/logout', (req, res) => {
   res.render('logout', { layout: null });
 });
 
 
-app.get('/menu', function (req, res) {
-	res.render('dashboard');
+app.get('/menu', (req, res) => {
+  res.render('dashboard');
 });
 
-app.get('/menu/api/v1/menu', function (req, res) {
-      res.send(foodList);
+app.get('/menu/api/v1/menu', (req, res) => {
+  res.send(foodList);
 });
 
-app.get('/api/v1/menu', function (req, res) {
-  
+app.get('/api/v1/menu', (req, res) => {
   jwt.verify(req.token, 'tre-lala', (err, authData) => {
-    if(err){
+    if (err) {
       res.sendStatus(403);
-    }else {
+    } else {
       res.send(foodList);
     }
-  })
-
+  });
 });
 
 // html for getting all orders by a user
-app.get('/orders', function (req, res) {
-	res.render('history');
+app.get('/orders', (req, res) => {
+  res.render('history');
 });
 // appi for getting all orders made by a user
 app.get('/api/v1/orders/:user', (req, res) => {
   let order = [];
-    jwt.verify(req.token, 'tre-lala', (err, authData) => {
-      if(err){
-        res.sendStatus(403);
-      }else {
-        orders.forEach(function (element, index) {
+  jwt.verify(req.token, 'tre-lala', (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      orders.forEach((element, index) => {
         if (element.user === req.params.user) {
           order = order.concat(element);
-          }
-        });
-        res.send(order);
-    }
-  })
-});
-
-//html for getting specific orders using order id, its api is below it
-app.get('/orders/:id', function (req, res) {
-	res.render('getorder');
-});
-
-app.get('/api/v1/order/:id', function (req, res) {
-   let order = [];
-
-   jwt.verify(req.token, 'tre-lala', (err, authData) => {
-    if(err){
-      res.sendStatus(403);
-    }else {
-          orders.forEach(function (element, index) {
-            if (element.orderID === req.params.id) {
-              order = order.concat(element);
-            }
-          });
+        }
+      });
       res.send(order);
     }
-  })
-
+  });
 });
 
+// html for getting specific orders using order id, its api is below it
+app.get('/orders/:id', (req, res) => {
+  res.render('getorder');
+});
+
+app.get('/api/v1/order/:id', (req, res) => {
+  let order = [];
+
+  jwt.verify(req.token, 'tre-lala', (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      orders.forEach((element, index) => {
+        if (element.orderID === req.params.id) {
+          order = order.concat(element);
+        }
+      });
+      res.send(order);
+    }
+  });
+});
 
 // PLACE AN ORDER
 app.post('/api/v1/placeOrder/:user', (req, res) => {
-  console.log(req.body);
+//  console.log(req.body);
 
-    jwt.verify(req.token, 'tre-lala', (err, authData) => {
-    if(err){
+ jwt.verify(req.token, 'tre-lala', (err, authData) => {
+    if (err) {
       res.sendStatus(403);
-    }else {
-        let orderId = Math.floor(Math.random() * 12345);
-        const newOrder = {
-          orderID: orderId + req.params.user, user: req.params.user, order: req.body, status: 'pending'
-        };
-        orders.push(newOrder);
-        res.status(201)
-        res.send(orders);
-      }
-  })
-
+    } else {
+      const orderId = Math.floor(Math.random() * 12345);
+      const newOrder = {
+        orderID: orderId + req.params.user, user: req.params.user, order: req.body, status: 'pending'
+      };
+      orders.push(newOrder);
+      res.status(201);
+      res.send(orders);
+    }
+  });
 });
 
 
-app.get('/messages', function (req, res) {
-	res.render('messages');
+app.get('/messages', (req, res) => {
+  res.render('messages');
 });
 
-app.get('/api/v1/messages/:user', function (req, res) {
-   let msgs = [];
-
-      jwt.verify(req.token, 'tre-lala', (err, authData) => {
-        if(err){
-          res.sendStatus(403);
-        }else {
-          messagesFromAdmin.forEach(function (element, index) {
-            if (element.receiver === req.params.user) {
-              msgs = msgs.concat(element);
-            }
-        });
-        res.send(msgs);
-      }
-  })
-
+app.get('/api/v1/messages/:user', (req, res) => {
+  let msgs = [];
+  jwt.verify(req.token, 'tre-lala', (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      messagesFromAdmin.forEach((element, index) => {
+        if (element.receiver === req.params.user) {
+          msgs = msgs.concat(element);
+        }
+      });
+      res.send(msgs);
+    }
+  });
 });
 
 app.post('/api/v1/messages/:user', (req, res) => {
-
-        jwt.verify(req.token, 'tre-lala', (err, authData) => {
-        if(err){
-          res.sendStatus(403);
-        }else {
-          const newMsg = {
-            sender: req.body.sender, message: req.body.message
-          };
-          messagesToAdmin.push(newMsg);
-          res.send(newMsg);
-      }
-  })
-
+  jwt.verify(req.token, 'tre-lala', (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      const newMsg = {
+        sender: req.body.sender, message: req.body.message
+      };
+      messagesToAdmin.push(newMsg);
+      res.send(newMsg);
+    }
+  });
 });
 
-//ADMIN
+// ADMIN
 app.get('/admin', (req, res) => {
   res.render('adminsignin', { layout: null });
 });
 
 app.post('/admin', (req, res) => {
-  let result = { userFound: false };
+  const result = { userFound: false };
   const signInUser = {
     username: req.body.uname, password: req.body.pword
   };
-  adminUsers.forEach(function (element) {
+  adminUsers.forEach((element) => {
     if ((element.username === signInUser.username) && (element.password === signInUser.password)) {
       result.userFound = true;
-      jwt.sign( { signInUser }, 'admin-sec-key', (err, token) => {
+      jwt.sign({ signInUser }, 'admin-sec-key', (err, token) => {
         result.token = token;
         res.send(result);
-      })
+      });
     }
   });
 
   if (!result.userFound) {
     res.send(result);
   }
-
 });
 
 
-app.get('/admin/admindashboard', function (req, res) {
-	res.render('admindashboard', { layout: 'admin' });
+app.get('/admin/admindashboard', (req, res) => {
+  res.render('admindashboard', { layout: 'admin' });
 });
 
 app.get('/api/v1/admin/orders', (req, res) => {
-    jwt.verify(req.token, 'admin-sec-key', (err, authData) => {
-    if(err){
+  jwt.verify(req.token, 'admin-sec-key', (err, authData) => {
+    if (err) {
       res.sendStatus(403);
-    }else {
+    } else {
       res.send(orders);
     }
-  })
+  });
 });
 
 
@@ -287,191 +281,188 @@ app.get('/admin/userorders/:order', (req, res) => {
 app.get('/api/v1/admin/userorders/:order', (req, res) => {
   let order = [];
 
-    jwt.verify(req.token, 'admin-sec-key', (err, authData) => {
-    if(err){
+  jwt.verify(req.token, 'admin-sec-key', (err, authData) => {
+    if (err) {
       res.sendStatus(403);
-    }else {
-        orders.forEach(function (element, index) {
+    } else {
+      orders.forEach((element, index) => {
         if (element.orderID === req.params.order) {
           order = order.concat(element);
         }
       });
       res.send(order);
     }
-  })
+  });
 });
 
 
-//Update status of an order
+// Update status of an order
 app.put('/api/v1/admin/orders/:id', (req, res) => {
   let order = [];
   jwt.verify(req.token, 'admin-sec-key', (err, authData) => {
-    if(err){
+    if (err) {
       res.sendStatus(403);
-    }else {
-        orders.forEach(function (element, index) {
-          if (element.orderID === req.params.id) {
-            element.status = req.body.status;
-            order = order.concat(element);
-          }
-        });
-        res.send(order);
+    } else {
+      orders.forEach((element, index) => {
+        if (element.orderID === req.params.id) {
+          element.status = req.body.status;
+          order = order.concat(element);
+        }
+      });
+      res.send(order);
     }
-  })
+  });
 });
 
-//FOOD LIST
+// FOOD LIST
 app.get('/admin/foodlist', (req, res) => {
   res.render('adminfoodlist', { layout: 'admin' });
 });
 
 app.get('/api/v1/admin/foodlists', (req, res) => {
-    jwt.verify(req.token, 'admin-sec-key', (err, authData) => {
-    if(err){
+  jwt.verify(req.token, 'admin-sec-key', (err, authData) => {
+    if (err) {
       res.sendStatus(403);
-    }else {
+    } else {
       res.send(foodList);
     }
-  })
+  });
 });
 
 
-//ADD FOOD
+// ADD FOOD
 app.get('/admin/addfood', (req, res) => {
   res.render('adminaddfood', { layout: 'admin' });
 });
 
 app.post('/api/v1/admin/addfood', (req, res) => {
-
-      jwt.verify(req.token, 'admin-sec-key', (err, authData) => {
-    if(err){
+  jwt.verify(req.token, 'admin-sec-key', (err, authData) => {
+    if (err) {
       res.sendStatus(403);
-    }else {
-        const newFood = {
-          foodName: req.body.foodName, foodPrice: req.body.foodPrice, foodDesc: req.body.foodDesc
-        };
-        foodList.push(newFood);
-        res.status(201);
-        res.send(foodList);
+    } else {
+      const newFood = {
+        foodName: req.body.foodName, foodPrice: req.body.foodPrice, foodDesc: req.body.foodDesc
+      };
+      foodList.push(newFood);
+      res.status(201);
+      res.send(foodList);
     }
-  })
-
+  });
 });
 
 
-//EDIT A FOOD DETAILS
+// EDIT A FOOD DETAILS
 app.get('/admin/editfood', (req, res) => {
   res.render('admineditfood', { layout: 'admin' });
 });
 
 app.get('/api/v1/admin/food/:name', (req, res) => {
   let order = [];
-      jwt.verify(req.token, 'admin-sec-key', (err, authData) => {
-    if(err){
+  jwt.verify(req.token, 'admin-sec-key', (err, authData) => {
+    if (err) {
       res.sendStatus(403);
-    }else {
-        foodList.forEach(function (element, index) {
-          if (element.foodName === req.params.name) {
-            order = order.concat(element);
-          }
-        });
-        res.send(order);
+    } else {
+      foodList.forEach((element, index) => {
+        if (element.foodName === req.params.name) {
+          order = order.concat(element);
+        }
+      });
+      res.send(order);
     }
-  })
+  });
 });
 
 app.put('/api/v1/admin/editfood', (req, res) => {
   let order = [];
-      jwt.verify(req.token, 'admin-sec-key', (err, authData) => {
-    if(err){
+  jwt.verify(req.token, 'admin-sec-key', (err, authData) => {
+    if (err) {
       res.sendStatus(403);
-    }else {
-        foodList.forEach(function (element, index) {
-    if (element.foodName === req.body.foodName) {
-      element.foodPrice = req.body.foodPrice;
-      element.foodDesc = req.body.foodDesc;
-      order = order.concat(element);
+    } else {
+      foodList.forEach((element, index) => {
+        if (element.foodName === req.body.foodName) {
+          element.foodPrice = req.body.foodPrice;
+          element.foodDesc = req.body.foodDesc;
+          order = order.concat(element);
+        }
+      });
+      res.send(order);
     }
   });
-  res.send(order);
-    }
-  })
 });
 
 
-//DELETE FOOD =================================================>
+// DELETE FOOD =================================================>
 app.get('/admin/deletefood', (req, res) => {
   res.render('admindeletefood', { layout: 'admin' });
 });
 app.delete('/api/v1/admin/deletefood', (req, res) => {
 
-      jwt.verify(req.token, 'admin-sec-key', (err, authData) => {
-    if(err){
+  jwt.verify(req.token, 'admin-sec-key', (err, authData) => {
+    if (err) {
       res.sendStatus(403);
-    }else {
-        foodList.forEach(function (element, index) {
-    if (element.foodName === req.body.foodName) {
-      foodList.splice(index, 1);
+    } else {
+      foodList.forEach((element, index) => {
+        if (element.foodName === req.body.foodName) {
+          foodList.splice(index, 1);
+        }
+      });
+      res.send(foodList);
     }
   });
-  res.send(foodList);
-    }
-  })
 });
 // ==========================================================>
 
 
-//GET MESSAGES
+// GET MESSAGES
 app.get('/admin/messages', (req, res) => {
   res.render('adminmessages', { layout: 'admin' });
 });
 
 app.get('/api/v1/admin/messages', (req, res) => {
-      jwt.verify(req.token, 'admin-sec-key', (err, authData) => {
-    if(err){
+  jwt.verify(req.token, 'admin-sec-key', (err, authData) => {
+    if (err) {
       res.sendStatus(403);
-    }else {
+    } else {
       res.send(messagesToAdmin);
     }
-  })
+  });
 });
 
 app.post('/api/v1/admin/messages', (req, res) => {
-  let result = { userFound : false };  
-
-      jwt.verify(req.token, 'admin-sec-key', (err, authData) => {
-    if(err){
+  const result = { userFound: false };
+  jwt.verify(req.token, 'admin-sec-key', (err, authData) => {
+    if (err) {
       res.sendStatus(403);
-    }else {
-          users.forEach(function (element) {
-    if ((element.username === req.body.receiver)) {
-      result.userFound = true;
-    }
-  });
+    } else {
+      users.forEach((element) => {
+        if ((element.username === req.body.receiver)) {
+          result.userFound = true;
+        }
+      });
 
-    if (result.userFound) {
-            const newMsg = {
+      if (result.userFound) {
+        const newMsg = {
           receiver: req.body.receiver, message: req.body.message
         };
         messagesFromAdmin.push(newMsg);
       }
 
-  res.send(result);
+      res.send(result);
     }
-  })
+  });
 });
 
 // custom 404 page
-app.use(function(req, res){
+app.use((req, res) => {
   res.type('text/plain');
   res.status(404);
   res.send('404 - Not Found');
 });
 
-app.listen(app.get('port'), function(){
-console.log( 'Express started on http://localhost:' +
-app.get('port') + '; press Ctrl-C to terminate.' );
+app.listen(app.get('port'), () =>{
+console.log( `Express started on http://localhost:
+${app.get('port')}; press Ctrl-C to terminate.`);
 });
 
-//app.listen(app.get('port'), 'localhost');
+// app.listen(app.get('port'), 'localhost');
 module.exports = app;
